3baf74d4f4d4928178f46deb0c339a0529ffd23f
Tomas Woods <tomaswoods@gmail.com>
2014-12-20 00:30:37 +0000

    Add missing files

--- js/index.js
+++ js/index.js
@@ -53,31 +53,47 @@ var app = {
 
     scan: function() {
         console.log('scanning');
+        var reader = new FileReader();
+
+        // Closure to capture the file information.
+        reader.onload = (function (theFile) {
+            return function (e) {
+                JsonObj = e.target.result
+                console.log(JsonObj);
+                var parsedJSON = JSON.parse(JsonObj);
+                var scanner = cordova.require("cordova/plugin/BarcodeScanner");
+
+                scanner.scan(function (result) {
+                    
+                    alert("We got a barcode\n" +
+                    "Result: " + result.text + "\n" +
+                    "Format: " + result.format + "\n" +
+                    "Cancelled: " + result.cancelled);
+                    if (parsedJSON[result.text]) {
+                        alert(parsedJSON[result.text].title + " " + parsedJSON[result.text].Manufacturer);
+                    }
+                    console.log("Scanner result: \n" +
+                         "text: " + result.text + "\n" +
+                         "format: " + result.format + "\n" +
+                         "cancelled: " + result.cancelled + "\n");
+                    document.getElementById("info").innerHTML = result.text;
+                    console.log(result);
+                    /*
+                    if (args.format == "QR_CODE") {
+                        window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
+                    }
+                    */
+
+                }, function (error) {
+                    console.log("Scanning failed: ", error);
+                });
+
+            };
+        })(f);
+
+        // Read in JSON as a data URL.
+        reader.readAsDataURL("products.json");
         
-        var scanner = cordova.require("cordova/plugin/BarcodeScanner");
-
-        scanner.scan( function (result) { 
-
-            alert("We got a barcode\n" + 
-            "Result: " + result.text + "\n" + 
-            "Format: " + result.format + "\n" + 
-            "Cancelled: " + result.cancelled);  
-
-           console.log("Scanner result: \n" +
-                "text: " + result.text + "\n" +
-                "format: " + result.format + "\n" +
-                "cancelled: " + result.cancelled + "\n");
-            document.getElementById("info").innerHTML = result.text;
-            console.log(result);
-            /*
-            if (args.format == "QR_CODE") {
-                window.plugins.childBrowser.showWebPage(args.text, { showLocationBar: false });
-            }
-            */
-
-        }, function (error) { 
-            console.log("Scanning failed: ", error); 
-        } );
     },
 
     encode: function() {
